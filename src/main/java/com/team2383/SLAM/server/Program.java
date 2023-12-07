5110ef205f47c586f0c0c8bff98c77587480a4dd
RisingNinjas16391 <106979512+Team-2383@users.noreply.github.com>
2023-12-07 16:49:22 -0500

    Add SLAM code and wpilib packages

---
+++ src/main/java/com/team2383/SLAM/server/Program.java
@@ -0,0 +1,31 @@
+package com.team2383.SLAM.server;
+
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.NetworkTablesJNI;
+import edu.wpi.first.util.CombinedRuntimeLoader;
+
+import java.io.IOException;
+
+import edu.wpi.first.cscore.CameraServerCvJNI;
+import edu.wpi.first.cscore.CameraServerJNI;
+import edu.wpi.first.math.WPIMathJNI;
+import edu.wpi.first.util.WPIUtilJNI;
+
+/**
+ * Program
+ */
+public class Program {
+    public static void main(String[] args) throws IOException {
+        NetworkTablesJNI.Helper.setExtractOnStaticLoad(false);
+        WPIUtilJNI.Helper.setExtractOnStaticLoad(false);
+        WPIMathJNI.Helper.setExtractOnStaticLoad(false);
+        CameraServerJNI.Helper.setExtractOnStaticLoad(false);
+        CameraServerCvJNI.Helper.setExtractOnStaticLoad(false);
+
+        CombinedRuntimeLoader.loadLibraries(Program.class, "wpiutiljni", "wpimathjni", "ntcorejni", "cscorejni");
+
+        NetworkTableInstance.getDefault().setServer(new String[] {"10.99.83.2", "127.0.0.1"}, new int[] {0, 0});
+
+        
+    }
+}
